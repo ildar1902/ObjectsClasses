8b67d6fbea5e2dc6b6ace6547ea8d8050fa114b8
Home <ildar86@live.ru>
2022-11-21 23:32:41 +0500

    выполнено ДЗ

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    String name;
+    Author author;
+    int publishingYear;
+
+    public Book(String name, Author author, int publishingYear) {
+        this.name = name;
+        this.author = author;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+}
